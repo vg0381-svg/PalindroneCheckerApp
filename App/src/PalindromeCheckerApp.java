58854d9a91d9f239f3c7b873869ab33f869da5cd
xri413 <gaddamchakri1@gmail.com>
2026-02-23 09:53:51 +0530

    vyshak added US-1

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+/*
+author:vyshak
+version:1.0
+ */
+
+
+public class PalindromeCheckerApp {
+    public static void main(String [] args){
+        System.out.println("welcome to Palindrome Checker App Management System");
+        System.out.println("Version: 1.0");
+
+
+    }
+
+}
